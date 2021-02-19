464c84c1968fb7ab86a7cd093f3834c5e4c37f9c
rezagw <mrgalih.w@gmail.com>
2021-02-19 15:34:31 +0700

    modified app.js

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(methodOverride("_method"));
 app.use(
   session({
